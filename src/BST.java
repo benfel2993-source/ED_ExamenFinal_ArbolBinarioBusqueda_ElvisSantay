9184356a67741c5f8186f1e4369f1ed1a52be3f2
Elvis Santay <benfel_2993@hotmail.com>
2026-05-16 23:18:32 -0600

    Update 3

--- src/BST.java
+++ src/BST.java
@@ -5,4 +5,32 @@ class BST {
     public BST() {
         raiz = null;
     }
+
+    // =========================
+    // INSERTAR
+    // =========================
+    public void insert(int valor) {
+        raiz = insertarRec(raiz, valor);
+    }
+
+    private Nodo insertarRec(Nodo actual, int valor) {
+
+        // Caso base: espacio vacío
+        if (actual == null) {
+            return new Nodo(valor);
+        }
+
+        // Ir a la izquierda
+        if (valor < actual.valor) {
+            actual.izquierda = insertarRec(actual.izquierda, valor);
+        }
+
+        // Ir a la derecha
+        else if (valor > actual.valor) {
+            actual.derecha = insertarRec(actual.derecha, valor);
+        }
+
+        // devolver nodo actualizado
+        return actual;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("🔥 EJECUTANDO MAIN CORRECTO 🔥");
+
         BST arbol = new BST();
 
-        System.out.println("Árbol creado correctamente 🚀");
+        arbol.insert(10);
+        arbol.insert(5);
+        arbol.insert(20);
+
+        System.out.println("FIN DEL PROGRAMA");
     }
 }
